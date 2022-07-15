98f77ade548c9d75255f565a6c9efdde44e18c6b
Luke Hinds <lhinds@redhat.com>
2022-07-15 12:16:02 +0100

    Add some debug

--- src/main.rs
+++ src/main.rs
@@ -10,8 +10,12 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let ret_token = env::var("ACTIONS_ID_TOKEN_REQUEST_TOKEN");
     let req_url = env::var("ACTIONS_ID_TOKEN_REQUEST_URL");
     // if we don't have both, error
+    print!("Checking for ACTIONS_ID_TOKEN_REQUEST_TOKEN / ACTIONS_ID_TOKEN_REQUEST_TOKEN...");
     if ret_token.is_err() || req_url.is_err() {
         panic!("ACTIONS_ID_TOKEN_REQUEST_TOKEN and ACTIONS_ID_TOKEN_REQUEST_URL must be set");
+    } else {
+        println!("ACTIONS_ID_TOKEN_REQUEST_TOKEN: {}", ret_token.as_ref().unwrap());
+        println!("ACTIONS_ID_TOKEN_REQUEST_URL: {}", req_url.as_ref().unwrap());
     }
 
     let client = reqwest::blocking::Client::new();
